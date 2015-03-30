e7953fe5e3fb26c7393628ba7170db048b0bfd86
Eric Murphy <murph141@purdue.edu>
2015-03-29 21:13:06 -0400

    Fix

--- Sources/font.h
+++ Sources/font.h
@@ -1,7 +1,7 @@
 #ifndef Cube_font_h
 #define Cube_font_h
 
-void font_getchar (char chr, unsigned char dst[5]);
-void font_getpath (unsigned char path, unsigned char *destination, int length);
+void font_getchar(char chr, unsigned char dst[5]);
+void font_getpath(unsigned char path, unsigned char *destination, int length);
 
 #endif
